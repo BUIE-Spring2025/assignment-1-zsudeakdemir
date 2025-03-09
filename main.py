3918caea58005e86f39e13bfede812689f718604
zsudeakdemir <akdemirsude231@hotmail.com>
2025-03-09 22:28:30 +0300

    Assignment1-ZeynepSudeAkdemir

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+    roman_dictionary = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),(100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),(10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+    result = ""
+    for value, roman_symbol in roman_dictionary:
+        while num >= value:
+            result = result + roman_symbol
+            num = num - value
+    return result
